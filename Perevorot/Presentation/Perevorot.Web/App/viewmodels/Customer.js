f119fbb8f7bbd296b4e8320c3765f1a52f23ff1c
Vladimir Rusanov <rusanov.vladimir@gmail.com>
2013-12-18 22:24:05 +0200

    Clean-up solution, installed Durandal from NuGet. Started to Work on Adding New Customer.

---
+++ Perevorot/Presentation/Perevorot.Web/App/viewmodels/Customer.js
@@ -0,0 +1,20 @@
+﻿define(["knockout"], function (ko) {
+    var _self;
+    var ctor = function () {
+        _self = this;
+        _self.counter = ko.observable(0);
+        _self.counter("1");
+    };
+    
+    this.createNewCustomer = function () {
+        _self.counter("2");
+    };
+
+  
+    //Note: This module exports a function. That means that you, the developer, can create multiple instances.
+    //This pattern is also recognized by Durandal so that it can create instances on demand.
+    //If you wish to create a singleton, you should export an object instead of a function.
+    //See the "flickr" module for an example of object export.
+
+    return ctor;
+});
--- Perevorot/Presentation/Perevorot.Web/App/viewmodels/shell.js
+++ Perevorot/Presentation/Perevorot.Web/App/viewmodels/shell.js
@@ -9,7 +9,8 @@
         activate: function () {
             router.map([
                 { route: '', title: 'Welcome', moduleId: 'viewmodels/welcome', nav: true },
-                { route: 'perevorot', title: 'Perevorot', moduleId: 'viewmodels/perevorot', nav: true }
+                { route: 'perevorot', title: 'Perevorot', moduleId: 'viewmodels/perevorot', nav: true },
+                { route: 'Customer', title: 'Customer', moduleId: 'viewmodels/customer', nav: true }
             ]).buildNavigationModel();
 
             return router.activate();
