bb12765d00b9b36a8e9a8d8656ff8417da87373a
DD <newddg1986@yahoo.com>
2013-12-21 20:45:47 +0200

    added localization support through i18next library.

--- Perevorot/Presentation/Perevorot.Web/App/viewmodels/shell.js
+++ Perevorot/Presentation/Perevorot.Web/App/viewmodels/shell.js
@@ -1,4 +1,4 @@
-﻿define(['plugins/router', 'durandal/app'], function (router, app) {
+﻿define(['plugins/router', 'durandal/app', 'i18next', 'jquery'], function (router, app, i18n, $) {
     return {
         router: router,
         search: function () {
@@ -6,13 +6,17 @@
             //You can add custom options too. Also, it returns a promise for the user's response.
             app.showMessage('Search not yet implemented...');
         },
+        languages: ['en', 'ro'],
+        changeLanguage: function (data) {
+            document.cookie = 'lang=' + data;
+            location.reload();
+        },
         activate: function () {
             router.map([
                 { route: '', title: 'Welcome', moduleId: 'viewmodels/welcome', nav: true },
                 { route: 'perevorot', title: 'Perevorot', moduleId: 'viewmodels/perevorot', nav: true },
                 { route: 'Customer', title: 'Customer', moduleId: 'viewmodels/customer', nav: true }
             ]).buildNavigationModel();
-
             return router.activate();
         }
     };
--- Perevorot/Presentation/Perevorot.Web/App/viewmodels/welcome.js
+++ Perevorot/Presentation/Perevorot.Web/App/viewmodels/welcome.js
@@ -1,6 +1,6 @@
-﻿define(function () {
+﻿define(['i18next'], function (i18n) {
     var ctor = function () {
-        this.displayName = 'Welcome to the Durandal Starter Kit!';
+        this.displayName = i18n.t('welcome:welcomeMessage');
         this.description = 'Durandal is a cross-device, cross-platform client framework written in JavaScript and designed to make Single Page Applications (SPAs) easy to create and maintain.';
         this.features = [
             'Clean MV* Architecture',
